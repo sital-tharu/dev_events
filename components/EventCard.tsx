e30363024017c95ae540ffea48f66bea59ce16d2
sital-tharu <sitatharu769@gmail.com>
2025-11-07 22:26:40 +0530

    creating reusable eventCard

---
+++ components/EventCard.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface Props {
+    title : string;
+    image: string;
+}
+
+
+const EventCard = ({ title, image}: Props) => {
+    return (
+        <Link href={`/events`} id ="event-card">
+            <Image src={image} alt={title} width={410} height={300} className="poster"/>
+
+            <p className= "title" ></p>
+        </Link>
+    )
+  return <div>EventCard</div>;
+};
+
+export default EventCard;
