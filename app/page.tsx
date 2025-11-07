1c648ab5724ce2db829647ec036fe8bf289c56d5
sital-tharu <sitatharu769@gmail.com>
2025-11-07 18:56:06 +0530

    Creaing Explore Events button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import ExploreBtn from "@/components/ExploreBtn";
 
 const page = () => {
   return (
       <section>
     <h1 className="text-center">The Hub for Every Dev<br />Event you can't miss</h1>
 
-      <p className = 'text-center mt-5'>Hackathon , Meetups and Conferences , All in one place </p>
+      <p className = 'text-center mt-5'>Hackathon , Meetups and Conferences , All in one place
+      </p><ExploreBtn/>
+          <div className ='mt-20 space-y-7'> </div>
       </section>
   )
 }
---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Image from 'next/image';
+
+const ExploreBtn = () => {
+  return <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={() => console.log('pressed')}>ExploreBtn
+  <a href ="#events">
+      Eplore Events
+      <Image src="/icons/arrow-down.svg" alt= "arrow-down" width={24} height={24} />
+  </a>
+  </button>;
+};
+
+export default ExploreBtn;
